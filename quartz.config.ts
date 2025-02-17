6ff8cc460b3531fb010178813ae427e575d0841b
ARYN <53664593+ARYN-27@users.noreply.github.com>
2025-02-18 00:13:40 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
     //},
     locale: "en-US",
     baseUrl: "patternlanguage.cc",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "*.pdf", "*.excalidraw"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
